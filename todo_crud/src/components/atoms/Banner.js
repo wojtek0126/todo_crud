e818658174b65e6b61c3f3ecfc6478cf0e390e28
wojtek0126 <41778298+wojtek0126@users.noreply.github.com>
2021-05-23 18:13:12 +0200

    banner ready to fit background image of blackboard

--- todo_crud/src/components/atoms/Banner.js
+++ todo_crud/src/components/atoms/Banner.js
@@ -1,19 +1,37 @@
 /** @jsxRuntime classic */
 /** @jsx jsx */  
+import { backgroundColor } from 'styled-system';
 import { jsx, Textarea } from 'theme-ui';
 import { Flex } from "theme-ui";
 
 
 const Banner = ({imageUrl}) => {
   return ( 
-      <Flex sx={{
-        flexDirection: 'row',
-        justifyContent: 'center',
-        alignItems: 'center',  
-        height: '30vw',
-        // width: '90vw'
-    }}>
-        <Textarea />
+    <Flex
+    sx={{
+      position: 'absolute',
+      zIndex: 1,
+      maxHeight: '30vh',
+      maxWidth: '100vw',
+      width: '-webkit-fill-available',
+      height: '29vh',      
+      flexDirection: 'column',
+      background: 'box',     
+      backgroundColor: 'boxBackground',
+      color: 'text',
+      border: '2px solid',
+      borderColor: 'boxBorder',
+      borderRadius: 4,
+      fontSize: 4,
+      margin: 3,
+      padding: 3,
+    }}
+  >
+        <Textarea sx={{height: '-webkit-fill-available',
+                       width: '-webkit-fill-available',
+                       maxWidth: '100%',  
+                       backgroundColor: 'inputBackground'                      
+                    }} />
           {/* <img src={imageUrl} />                    */}
       </Flex>         
   );
@@ -21,3 +39,11 @@ const Banner = ({imageUrl}) => {
 
 
 export default Banner;
+
+
+    // z-index: 1;
+    //  
+    // width: -webkit-fill-available;
+    // height: 29vh;
+    // margin-top: -10px;
+    // margin-bottom: 13px;
