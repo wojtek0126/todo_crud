ed2bd69f82312a2b9c0bc733ccfe7f49023de568
wojtek0126 <41778298+wojtek0126@users.noreply.github.com>
2021-07-01 14:02:02 +0200

    old version with lag

--- todo_crud/src/functions/functionStorage.js
+++ todo_crud/src/functions/functionStorage.js
@@ -1,5 +1,5 @@
 
-//switch button text, add it to onClick handler for buttons, 
+// switch button text, add it to onClick handler for buttons, 
 export const switchBtnTxt = (setUpdateState, text1, text2, delay = 1800) => {
     setUpdateState(text2);
     setTimeout(() => {
@@ -7,7 +7,7 @@ export const switchBtnTxt = (setUpdateState, text1, text2, delay = 1800) => {
     }, delay); 
 }
 
-//formatted current date and time, for create and edit tasks
+// formatted current date and time, for create and edit tasks
 export const timeStampFormatted = () => {
   const timestamp = Date.now(); // This would be the timestamp you want to format
   const formatted = new Intl.DateTimeFormat('en-US', {year: 'numeric', month: '2-digit',day: '2-digit',
